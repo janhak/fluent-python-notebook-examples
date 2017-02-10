0c95ce5364130372918110c367204461aba2bbc4
Jan Chwiejczak <jchwiejczak@gmail.com>
2017-02-10 22:35:04 +0000

    Basic of python string and bytes objects

---
+++ 4-text-versus-bytes/memoryview.py
@@ -0,0 +1,12 @@
+import struct
+fmt ='<3s3sHH'
+
+with open('academy.gif', 'rb') as fp:
+    img = memoryview(fp.read())
+
+header = img[:10]
+print(bytes(header))
+print(struct.unpack(fmt, header))
+
+del header
+del img
