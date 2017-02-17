c43bce830e67066fd829b4c6ba85ea96645ea751
Jan Chwiejczak <jchwiejczak@gmail.com>
2017-02-17 00:04:28 +0000

    Intro to decorators

---
+++ 7-Function-decorators-and-closures/registration.py
@@ -0,0 +1,39 @@
+"""
+The function decorators are executed as soon as the module is
+imported, but the decorated functions run only when they are explicitely
+invoked.
+"""
+registry = []
+
+
+def register(func):
+    print('running register(%s)' % func)
+    registry.append(func)
+    return func
+
+
+@register
+def f1():
+    print('running f1()')
+
+
+@register
+def f2():
+    print('running f2()')
+
+
+def f3():
+    print('running f3()')
+
+
+def main():
+    print('running main()')
+    print('registry ->', registry)
+    f1()
+    f2()
+    f3()
+
+if __name__=='__main__':
+    main()
+
+
